af1f4c922314cbb499ac08baf2a35d91bcc01960
Miranda McDonald <mirandagrace.dev@gmail.com>
2021-01-22 21:29:33 -0500

    created the db  tables

--- db/schema.sql
+++ db/schema.sql
@@ -1,14 +1,30 @@
+-- create db
 DROP DATABASE IF EXISTS employee_db;
 
 CREATE DATABASE employee_db;
 
 USE employee_db;
 
+-- create tables
 CREATE TABLE employee (
     id INTEGER NOT NULL AUTO_INCREMENT,
     first_name VARCHAR(20),
     last_name VARCHAR(20),
     role_id INTEGER,
     manager_id INTEGER,
+    PRIMARY KEY (id)
+);
+
+CREATE TABLE department (
+    id INTEGER NOT NULL AUTO_INCREMENT,
+    name VARCHAR(30),
+    PRIMARY KEY (id)
+);
+
+CREATE TABLE role (
+    id INTEGER NOT NULL AUTO_INCREMENT,
+    title VARCHAR(30),
+	salary DECIMAL(10,0),
+    department_id INTEGER,
     PRIMARY KEY (id)
 );
